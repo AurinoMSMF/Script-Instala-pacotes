3089921f4125297fe529aca4ad7f2470056b47df
Aurino <aurinodoc@gmail.com>
2023-05-19 14:22:21 -0300

    Melhoria

--- script_meus_pacotes_padr�o.sh
+++ script_meus_pacotes_padr�o.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo -e "====================SCRIPT DE INSTALAÇÃO DE PACOTES BÁSICOS===================="                                         
 [[ $UID -eq 0 ]] && { echo "Usuário logado como root. Iniciando processo de instalação de pacotes." ;                             \
-sleep 1 ; apt install xorg i3 pavucontrol vim pcmanfm openssh-server  neofetch baobab nitrogen flatpak ;                          \
+sleep 1 ; apt install xorg i3 pavucontrol vim pcmanfm openssh-server git discord neofetch baobab nitrogen flatpak ;              \
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo ;                                         \
 apt install snapd ;                                                                                                               \
 snap install spotify ;                                                                                                            \
