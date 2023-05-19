d2a2c81d8d067d892332fff85545cf7a26c8475c
Aurino <aurinodoc@gmail.com>
2023-05-19 14:08:13 -0300

    subindo script

---
+++ script_meus_pacotes_padr�o.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+echo -e "====================SCRIPT DE INSTALAÇÃO DE PACOTES BÁSICOS===================="                                         
+[[ $UID -eq 0 ]] && { echo "Usuário logado como root. Iniciando processo de instalação de pacotes." ;                             \
+sleep 1 ; apt install xorg i3 pavucontrol vim pcmanfm openssh-server  neofetch baobab nitrogen flatpak ;                          \
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo ;                                         \
+apt install snapd ;                                                                                                               \
+snap install spotify ;                                                                                                            \
+snap install code --classic ;                                                                                                     \
+snap install firefox ;                                                                                                            \
+echo -e "====================ATUALIZAÇÃO FINALIZADA====================" ;							  \
+exit 0 ;} || { echo -e "Ops... O script necessita de privilégios de superusuário para ser executado. Favor, entrar como root." ;   \
+ exit 1 ;}                                                                                                                        
